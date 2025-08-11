2f83bd2a370b4771e2bb292a9e67d26f72180351
preetys <preeti.sharma@axonichealth.com>
2025-08-11 15:39:12 +0530

    strings in python

---
+++ basicPython/practise2.py
@@ -0,0 +1,59 @@
+# name = input("Enter your name: ")
+# print("Hello,", name)
+
+# age = input("Enter your age: ")
+# print("You are", age, "years old.")
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter seconfd number:  "))
+# print("Sum:", num1+num2)
+
+
+# text = "Python Programming"
+
+# print(text[0])
+# print(text[1:4])
+# print(text[:6])
+# print(text[7:])
+# print(text[-4])
+
+msg = "  I am loving Python Programming   "
+# print(msg.upper())
+# print(msg.lower())
+# print(msg.strip())
+# print(msg.replace("Python", "AI"))
+# print(msg.split())
+# print(msg.find("Python"))
+# print(len(msg))
+
+
+# name = "Preeti"
+# age = 25
+# print(f"My name is {name} and I'm {age} years old")
+
+# name = input("Enter your name: ")
+
+# print("Upper", name.upper())
+
+
+# print("Lower", name.lower())
+# print(len(name))
+
+# sentence = input("Enter a sentence: ")
+# first_three = sentence[:3]
+# print(first_three)
+# last_three = sentence[-3:]
+# print(last_three)
+
+# reversed_sentence = sentence[::-1]
+# print(reversed_sentence)
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1+num2
+difference = num1-num2
+product = num1*num2
+average = sum/2
+print(f" Sum: {sum} \n Difference: {difference} \n Product: {product} \n Average: {average}")
