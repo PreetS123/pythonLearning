3f0901bcebfcbf80544c8f7c597024436c4cb03b
preetys <preeti.sharma@axonichealth.com>
2025-09-13 12:10:59 +0530

    strings in python

---
+++ beginnerPython/string_operations.py
@@ -0,0 +1,29 @@
+# name = "Pre'eti"
+name = "Pre'\"eti"
+# print(name)
+
+
+# slicing
+full_name = "Preeti Sharma"
+# print(full_name[2:4])
+
+
+# Falsy values in python
+# 0
+# ""
+# list, tuples and dist are false when empty
+
+
+# any
+# it is a method in python to check if any of the value is true or not
+bool1 = True
+bool2 = False
+output = any([bool1, bool2])
+# print(output)
+
+# all
+# It is similar but if all are True then only it will return True
+ingredient_purchased = True
+meal_cooked = False
+result = all([ingredient_purchased, meal_cooked])
+print(result)
---
+++ beginnerPython/variable_types.py
@@ -0,0 +1,67 @@
+name = "Preeti"
+
+# induntation is very important in python
+
+# checking the type of variable
+
+# print(type(name) == str)
+# print(isinstance(name, str))
+
+age = int("20")  # this is called type casting
+# print(isinstance(age, int))
+
+age = 2.9
+# print(isinstance(age, float))
+
+
+"""
+complex for complex numbers
+bool for booleans
+list for lists
+tuple for tuples
+range for ranges
+dict for dictionaries
+set for sets
+"""
+
+# operators in python
+# arithmetic operators: +, -, *, /, %, **, // (floor division)
+
+a = 1
+b = 2
+"""
+print(a == b)  # False
+print(a != b)  # True
+print(a > b)  # False
+print(a < b)   # True
+print(a >= b)  # False
+print(a <= b)  # True
+
+"""
+
+# logical operators: and, or, not
+"""
+print(0 or 1)  # 1
+print(False or 'hey')  # hey
+print('hi' or 'hello')  # hi
+print([] or False)  # False
+print(False or [])  # []
+
+
+print(0 and 1)  # 0
+print(1 and 0)  # 0
+print(False and 'hey')  # False
+print('hi' and 'hey')  # hey
+print([] and False)  # []
+print(False and [])  # False
+
+"""
+
+
+# membership operators: in, not in
+# identity operators: is, is not
+# bitwise operators: &, |, ^, ~, <<, >>, >>
+
+# assignment operators: =, +=, -=, *=, /=, %=, **=, //=
+# augmented assignment operators: +=, -=, *=, /=, %=, **=, //=
+# operator precedence: (), **, *, /, //, %, +, -, <, <=, >, >=, ==, !=, and, or, not
