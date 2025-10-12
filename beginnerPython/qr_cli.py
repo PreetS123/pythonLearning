e0e29c943fdd5f81583eba86d396ba2717179e99
PreetS123 <preetbhardwaj517@gmail.com>
2025-10-12 17:56:57 +0530

    abc

---
+++ beginnerPython/qr_cli.py
@@ -0,0 +1,5 @@
+import qrcode 
+
+
+myQR=qrcode.make("https://github.com/PreetS123")
+myQR.save("myqr.png")
--- oops_concept/01_classes.py
+++ oops_concept/01_classes.py
@@ -6,7 +6,16 @@ Class is a blueprint of creating an object
 """
 
 class Student:
-    name= "Preeti"
+    def __init__(self,name):
+        self.name= name
+        print("Student added in database")
 
-s1= Student()
+# creating object (instance / instance of class)
+s1= Student("Preeti")
 print(s1.name)
+
+
+# constructor 
+"""All classes have function called __init__(), 
+which is always executed when the class is being initiated."""
+# __init__ function
